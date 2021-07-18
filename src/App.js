9024b3302c214d11f3c4c549fb935523700f5a53
Vipin Tanna <6302771+vip-git@users.noreply.github.com>
2021-07-18 22:52:25 +0200

    Update App.js

--- src/App.js
+++ src/App.js
@@ -5,13 +5,15 @@ import MaterialJsonSchemaForm from 'react-jsonschema-form-material-ui/dist/bundl
 // Internals
 import schema from './simple/schema.json';
 import uiSchema from './simple/ui-schema.json';
-import formData from './simple/form-data.json';
+import givenFormData from '../simple/form-data.json';
 
 const App = () => {
+  const [formData, setFormData] = React.useState(givenFormData);
   return <MaterialJsonSchemaForm 
             schema={schema} 
             uiSchema={uiSchema} 
             formData={formData} 
+            onChange={({ formData }) => setFormData(formData)}
             onSubmit={(submittedData) => console.log('form submitted', submittedData)}
           />;
 };
